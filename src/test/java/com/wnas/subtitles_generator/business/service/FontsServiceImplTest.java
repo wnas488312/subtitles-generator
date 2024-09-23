28aeaaf36ef901f4372a4273c6e82bdf402a1bd9
wnas488312 <wiktor.naszydlowski@gmail.com>
2024-09-23 22:17:27 +0200

    Add test for loadCustomFonts method

--- src/test/java/com/wnas/subtitles_generator/business/service/FontsServiceImplTest.java
+++ src/test/java/com/wnas/subtitles_generator/business/service/FontsServiceImplTest.java
@@ -1,13 +1,19 @@
 package com.wnas.subtitles_generator.business.service;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import java.awt.*;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FontsServiceImplTest {
+    // Place name of added custom font here before enabling loadCustomFontsTest() test
+    private final static String AVAILABLE_FONT_NAME = "";
 
     @Test
     void getDefaultFontsNamesTest() {
@@ -18,5 +24,19 @@ class FontsServiceImplTest {
         assertThat(defaultFontsNames).contains("Arial");
         assertThat(defaultFontsNames).contains("Serif");
         assertThat(defaultFontsNames).contains("Times New Roman");
+    }
+
+    @Test
+    @Disabled
+    void loadCustomFontsTest() {
+        new FontsServiceImpl().loadCustomFonts();
+
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        Font[] allFonts = ge.getAllFonts();
+        final Optional<String> foundCustomFont = Arrays.stream(allFonts)
+                .map(Font::getFontName)
+                .filter(fontName -> fontName.startsWith(AVAILABLE_FONT_NAME))
+                .findFirst();
+        assertThat(foundCustomFont).isNotEmpty();
     }
 }
